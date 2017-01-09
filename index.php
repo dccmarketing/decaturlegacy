bfb53f4780c64c128f63de40a2fc76fd13f27631
Chris Wilcoxson <chris@slushman.com>
2017-01-09 15:57:17 -0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Decatur Legacy</title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<head>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Arvo:400,700,700italic' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' id='decatur-legacy-style-css' href='style.css' type='text/css' media='all' />
+
+</head>
+<body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N73TQQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N73TQQ');</script>
+<!-- End Google Tag Manager -->
+	<header>
+		<div class="wrap">
+			<div class="logo">
+				<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 267.84 180.324" enable-background="new 0 0 267.84 180.324" xml:space="preserve"><path fill="#ffffff" d="M4.58 165.461h7.29c10.376 0 12.76-7.896 12.76-11.822 0-6.404-4.067-11.824-12.993-11.824H4.58V165.461zM0 137.701h12.385c11.776 0 16.965 8.414 16.965 15.938 0 8.178-6.403 15.936-16.917 15.936H0V137.701zM45.359 156.957c-0.141-3.178-1.963-5.188-4.907-5.188 -3.178 0-5.095 1.869-5.655 5.188H45.359zM49.097 167.518c-2.524 1.686-4.813 2.479-8.364 2.479 -6.544 0-10.19-5.375-10.19-11.17 0-6.357 4.02-10.656 9.815-10.656 5.561 0 9.113 3.787 9.113 11.543H34.704c0.653 4.162 3.037 6.311 7.056 6.311 2.524 0 4.907-0.936 7.337-2.521V167.518zM67.508 154.061c-2.383-1.592-3.971-2.059-5.795-2.059 -3.784 0-6.401 2.852-6.401 7.104 0 4.346 2.803 6.965 7.104 6.965 1.776 0 3.457-0.469 5.562-1.402v4.111c-1.404 0.656-4.067 1.217-6.265 1.217 -6.261 0-10.655-4.346-10.655-10.516 0-6.916 4.019-11.311 10.376-11.311 2.431 0 4.066 0.562 6.074 1.449V154.061zM80.8 159.199c-3.554 1.215-6.639 2.432-6.639 4.908 0 1.73 1.263 2.943 2.991 2.943 1.31 0 2.525-0.654 3.648-1.729V159.199zM87.948 168.359c-1.916 1.262-2.71 1.637-4.018 1.637 -1.824 0-2.804-0.654-3.179-2.15 -1.821 1.447-3.739 2.15-5.655 2.15 -3.084 0-5.14-2.43-5.14-5.141 0-4.16 3.785-5.422 7.15-6.637l3.694-1.309v-1.123c0-2.615-1.263-3.689-3.788-3.689 -2.29 0-4.627 1.074-6.635 3.459v-4.537c1.635-1.914 3.924-2.85 6.964-2.85 4.252 0 7.617 2.15 7.617 6.918v10.795c0 0.795 0.28 1.074 0.794 1.074 0.42 0 1.263-0.42 2.196-1.119V168.359zM94.736 148.59h6.634v3.74h-6.634v10.748c0 2.479 1.774 3.18 2.989 3.18 1.495 0 3.039-0.514 4.72-1.543v3.881c-1.449 0.793-3.691 1.4-5.047 1.4 -4.534 0-6.824-2.803-6.824-6.59V152.33h-3.645v-0.467l7.807-7.619V148.59zM117.52 169.576v-2.666c-1.729 1.869-4.159 3.086-6.216 3.086 -4.253 0-7.243-3.039-7.243-8.367V148.59h4.158v13.32c0 2.945 1.45 4.627 4.301 4.627 1.868 0 3.645-1.029 5-2.943V148.59h4.158v20.986H117.52zM128.796 148.59v4.816h0.095c2.104-3.691 4.113-5.236 6.076-5.236 1.683 0 3.271 0.797 5.006 2.432l-2.203 3.598c-1.262-1.215-2.897-2.057-3.877-2.057 -2.853 0-5.096 2.852-5.096 6.451v10.98h-4.158V148.59H128.796z"/><polygon fill="#ffffff" points="156.189,165.461 170.584,165.461 170.584,169.576 151.609,169.576 151.609,137.703    156.189,137.703  "/><path fill="#ffffff" d="M185.712 156.957c-0.14-3.178-1.961-5.188-4.907-5.188 -3.178 0-5.094 1.869-5.651 5.188H185.712zM189.452 167.518c-2.524 1.686-4.814 2.479-8.366 2.479 -6.542 0-10.188-5.375-10.188-11.17 0-6.357 4.019-10.656 9.814-10.656 5.562 0 9.111 3.787 9.111 11.543h-14.768c0.654 4.162 3.041 6.311 7.058 6.311 2.523 0 4.908-0.936 7.338-2.521V167.518zM199.8 152.191c-2.289 0-4.159 1.727-4.159 3.783 0 2.289 1.682 3.834 4.205 3.834 2.338 0 4.019-1.635 4.019-3.928C203.864 153.871 201.995 152.191 199.8 152.191M198.726 170.602c-2.805 0-4.768 1.311-4.768 3.18 0 2.104 2.058 3.225 5.842 3.225 3.646 0 5.795-1.121 5.795-3.037C205.595 171.305 201.295 170.602 198.726 170.602M209.707 151.816h-4.299c1.637 1.635 2.195 2.803 2.195 4.58 0 1.727-0.98 3.738-1.962 4.719 -2.805 2.992-8.505 1.496-8.505 3.973 0 1.17 2.196 1.869 6.4 2.709 4.488 0.891 6.076 3.18 6.076 5.844 0 4.16-3.689 6.684-9.906 6.684 -5.655 0-9.676-2.664-9.676-6.355 0-3.086 1.638-4.908 5.281-5.795 -1.447-0.887-2.101-1.73-2.101-2.666 0-1.354 1.307-2.523 3.316-2.99v-0.092c-1.543-0.654-2.711-1.543-3.505-2.664 -0.747-1.076-1.123-2.385-1.123-3.928 0-4.484 3.507-7.244 9.162-7.244h8.645V151.816zM222.243 159.199c-3.55 1.215-6.635 2.432-6.635 4.908 0 1.73 1.261 2.943 2.988 2.943 1.309 0 2.525-0.654 3.646-1.729V159.199zM229.394 168.359c-1.918 1.262-2.711 1.637-4.02 1.637 -1.822 0-2.804-0.654-3.177-2.15 -1.823 1.447-3.74 2.15-5.655 2.15 -3.086 0-5.141-2.43-5.141-5.141 0-4.16 3.785-5.422 7.149-6.637l3.692-1.309v-1.123c0-2.615-1.26-3.689-3.786-3.689 -2.291 0-4.626 1.074-6.637 3.459v-4.537c1.636-1.914 3.927-2.85 6.964-2.85 4.255 0 7.618 2.15 7.618 6.918v10.795c0 0.795 0.28 1.074 0.795 1.074 0.421 0 1.261-0.42 2.196-1.119V168.359zM245.425 154.061c-2.383-1.592-3.974-2.059-5.795-2.059 -3.786 0-6.403 2.852-6.403 7.104 0 4.346 2.805 6.965 7.104 6.965 1.775 0 3.46-0.469 5.561-1.402v4.111c-1.401 0.656-4.066 1.217-6.261 1.217 -6.264 0-10.657-4.346-10.657-10.516 0-6.916 4.021-11.311 10.376-11.311 2.43 0 4.065 0.562 6.076 1.449V154.061z"/><polygon fill="#ffffff" points="263.167,148.59 257.746,160.414 252.09,148.59 247.369,148.59 255.501,165.088 248.352,180.324    252.979,180.324 267.84,148.59  "/><path fill="#A8CABA" d="M102.334 77.906c-2.492 0-4.52-1.682-4.52-3.765 0-2.077 2.028-3.764 4.52-3.764 2.49 0 4.517 1.687 4.517 3.764C106.852 76.224 104.825 77.906 102.334 77.906M141.641 53.147c4.176 0 7.562 2.822 7.562 6.306 0 3.479-3.387 6.297-7.562 6.297 -4.173 0-7.561-2.818-7.561-6.297C134.08 55.969 137.468 53.147 141.641 53.147M163.369 114.016c-4.303-8.252-11.496-19.789-22.246-30.359 -10.202 10.031-17.2 20.933-21.566 29.072 -6.694 0.264-13.164 0.732-19.334 1.385 9.062-17.051 21.374-28.799 33.73-36.879 -9.303-7.576-20.725-14.077-34.559-17.621 0 0 19.94 1.369 41.73 13.32 12.23-6.708 23.875-10.084 31.74-11.751 -9.84-9.017-24.763-14.77-41.472-14.77 -23.806 0-43.978 11.68-50.996 27.842 4.479 0.746 12.945 2.711 21.901 7.675 10.745-5.637 19.83-5.543 19.83-5.543 -20.749 8.473-31.282 25.995-34.931 33.336 1.72 1.877 3.649 3.635 5.753 5.264 -26.318 3.539-46.078 10.521-52.525 18.965 19.359-8.834 53.255-14.889 91.859-14.889 38.616 0 72.515 6.055 91.869 14.895C217.054 124.648 193.772 117.121 163.369 114.016M97.932 84.503c-4.886-4.013-10.763-7.529-17.745-9.78 -1.59 3.819-2.448 7.881-2.448 12.096 0 5.203 1.31 10.173 3.691 14.742C86.443 93.648 92.274 88.223 97.932 84.503M177.869 106.988c4.558-5.936 7.18-12.818 7.18-20.169 0-9.196-4.086-17.671-10.956-24.462 -9.983 3.709-18.537 8.963-25.8 14.876C158.85 84.136 169.375 93.713 177.869 106.988M190.937 7.223c4.947-0.9 7.419-1.48 11.707-2.834 -3.023 3.989-4.839 5.205-8.512 7.752 2.053 3.258 3.088 4.99 5.213 9.055 -4.372-3.13-6.558-3.834-10.865-5.22 -3.714 2.569-5.603 3.882-8.885 8.135 0.872-4.511 1.369-6.463 2.405-10.178 -4.278-1.359-6.396-1.991-10.482-4.926 4.485 0.046 7.008-0.113 11.977-0.679 1.034-3.374 1.567-4.972 2.615-8.328C188.048 2.908 189.002 4.286 190.937 7.223"/></svg>
+			</div><!-- .logo -->
+			<div class="header_creds">
+				Decatur Legacy | 125 North Water St, Suite 200 | Decatur, IL 62523 | 217.429.3000
+			</div><!-- .header_creds -->
+		</div><!-- .wrap -->
+	</header>
+	<section class="content">
+		<div class="wrap">
+			<div class="give_image halvsies">
+				<img src="give_live.png" />
+			</div>
+			<div class="content_text halvsies">
+				<p>Decatur Legacy is a membership organization made up of charitable organizations and professional advisors -accountants, attorneys, wealth managers, financial advisors - who encourage supporters, donors, volunteers, and clients to include a gift to charity in their wills or estate plans.</p>
+				<p>Leaving a legacy to your community allows people from all walks of life to make a difference even after they are gone.</p>
+				<p>To learn more about how you can leave a legacy to your favorite charity, contact any of the Decatur Legacy members, or call 217.429.3000.</p>
+			</div><!-- .content_text -->
+		</div><!-- .wrap -->
+	</section><!-- .content -->
+	<section class="members">
+		<div class="wrap">
+			<h4>Decatur Legacy Professional Members Include:</h4>
+			<div class="list">
+				<ul>
+					<li>Benjamin F. Edwards and Co.</li>
+					<li>BKD, LLP</li>
+					<li>Brechnitz Group of Raymond James</li>
+					<li>Busey Wealth Management</li>
+					<li>Consociate Dansig</li>
+					<li>Erickson, Davis, Murphy, Johnson & Walsh, Ltd.</li>
+					<li>First Mid-Illinois Bank & Trust</li>
+					<li>Foster Financial Services, Inc.</li>
+					<li>Hickory Point Bank and Trust</li>
+					<li>J.L. Hubbard Insurance and Bonds</li>
+					<li>May Cocagne & King P.C.</li>
+					<li>McGuire, Yuhas, Huffman & Buckley, P.C.</li>
+					<li>Midland States Bank Wealth Management</li>
+					<li>Reiss Financial Group, L.L.C.</li>
+					<li>Roberts Law Office</li>
+					<li>Samuels, Miller, Schroeder, Jackson & Sly</li>
+					<li>Sikich</li>
+					<li>Soy Capital Bank and Trust</li>
+					<li>Stifel Nicolaus</li>
+				</ul>
+			</div>
+		</div><!-- .wrap -->
+	</section><!-- .members -->
+	<section class="orgs">
+		<div class="wrap">
+			<h4>Decatur Legacy Member Organizations Include:</h4>
+			<div class="list">
+				<ul>
+					<li>American Red Cross</li>
+					<li>Baby TALK, Inc.</li>
+					<li>Big Brothers Big Sisters of Central Illinois</li>
+					<li>Boys and Girls Club of Decatur</li>
+					<li>Catholic Charities, Inc.</li>
+					<li>Children's Museum of Illinois</li>
+					<li>Community Foundation of Macon County</li>
+					<li>Community Health Improvement Center</li>
+					<li>Decatur Area Arts Council</li>
+					<li>Decatur Christian School</li>
+					<li>Decatur Day Care Center</li>
+					<li>Decatur Family YMCA</li>
+					<li>Decatur Memorial Foundation</li>
+					<li>Decatur Parks Foundation</li>
+					<li>Decatur Public Library</li>
+					<li>Decatur Public Schools Foundation</li>
+					<li>Decatur SCORE Chapter #296</li>
+					<li>DMCOC</li>
+					<li>Dove, Inc.</li>
+					<li>Education Coalition of Macon County</li>
+					<li>Friends of St. Mary’s Hospital</li>
+					<li>Futures Unlimited</li>
+					<li>Girl Scouts of Central Illinois</li>
+					<li>God’s Shelter of Love, Inc.</li>
+					<li>Good Samaritan Inn</li>
+					<li>Governor Oglesby Mansion, Inc.</li>
+					<li>Heritage Behavioral Health Center</li>
+					<li>Independence Pointe</li>
+					<li>James Millikin Homestead</li>
+					<li>Lincoln Trails Boy Scout Council</li>
+					<li>Lutheran School Association</li>
+					<li>Macon County CASA</li>
+					<li>Macon County Conservation Foundation</li>
+					<li>Macon Resources, Inc.</li>
+					<li>Millikin University</li>
+					<li>Northeast Community Fund</li>
+					<li>Partners In Education of Decatur & Macon County</li>
+					<li>Project Read</li>
+					<li>Richland Community College Foundation</li>
+					<li>Rock Springs Endowment Fund</li>
+					<li>St. Teresa Educational Foundation</li>
+					<li>Theatre 7</li>
+					<li>United Way of Decatur and Mid-Illinois</li>
+					<li>Webster Cantrell Hall</li>
+				</ul>
+			</div>
+		</div><!-- .wrap -->
+	</section><!-- .orgs -->
+	<footer>
+		<div class="wrap">
+			<div class="house">
+				<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 36 33.372" enable-background="new 0 0 36 33.372" xml:space="preserve"><path fill="#FFFFFF" d="M25.839,0.14l10.162,12.055l-2.318,0.022L24.521,1.353l-8.394,13.923L0,11.81v-1.213L6.889,0.001 L25.839,0.14z M19.935,27.756l4.744-1.664v-3.646l-4.744,1.466V27.756z M26.492,25.46l4.12-1.438v-3.405l-4.12,1.268V25.46z M7.196,25.208v-3.444l-3.922-1.209v3.29L7.196,25.208z M14.754,21.928v-3.739L10.14,17.05v3.566L14.754,21.928z M19.979,21.884 l4.739-1.36v-3.648l-4.739,1.164V21.884z M26.525,20.006l4.12-1.181V15.42l-4.12,1.011V20.006z M7.406,19.836V16.38l-3.945-0.966 v3.296L7.406,19.836z M24.593,2.748l8.151,9.656V26.62l-15.786,6.751V33.36l-2.204-0.945v-9.376l-4.619-1.368v8.768L0.88,26.477 L0.874,12.778l15.836,3.493L24.593,2.748z"/></svg>
+			</div>
+			<div class="copyright">
+				&copy; <?php echo date('Y'); ?> Decatur Legacy | 125 North Water St, Suite 200 | Decatur, IL 62523 | 217.429.3000
+			</div><!-- .copyright -->
+			<div class="credits">
+				Site designed and developed by DCC Marketing
+			</div><!-- .credits -->
+		</div><!-- .wrap -->		
+	</footer>
+</body>
+</html>
